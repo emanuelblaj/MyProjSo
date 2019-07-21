ec82fed0ff7fe9e2d96cc7b493b0379b2ca30f01
emanu <emanu@DESKTOP-12VDPMC>
2019-07-21 17:32:59 +0300

    Patterns Runnable and Singleton

---
+++ src/com/patterns/runnable/LongWrapper.java
@@ -0,0 +1,19 @@
+package com.patterns.runnable;
+
+public class LongWrapper {
+	private long l;
+	private Object key = new Object();
+	
+	public LongWrapper(long l) {
+		this.l = l;
+	}
+
+	public long getValue() {
+		return l;
+	}
+	public void incrementValue() {
+		synchronized(key) {
+			l = l + 1;
+		}
+	}
+}
